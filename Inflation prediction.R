9efef290c4a59d05ce7f732a9f2fab9577f16a32
michiel98fjhsad <94781488+michiel98fjhsad@users.noreply.github.com>
2022-02-18 10:07:12 +0100

    na.omit

--- Inflation prediction.R
+++ Inflation prediction.R
@@ -1,6 +1,8 @@
 # R script goes here
 head(fred_final$CPIAUCSL)
 
-ICfactors(fred_final)
-prcomp(fred_final)
-summary(fred_final$CPIAUCSL)
+f <- ICfactors(fred_final[,-106]) # selects 17 factors
+names(f)
+prcomp(na.omit(fred_final[,-106], rank. = 20 )) # CPIAUCSL is 106th column
+
+  
